1f6d0f155fb5cb5057853b14912267ed3f0b0634
hmiya6 <h.miya.work@gmail.com>
2023-11-04 23:05:04 +0900

    astronote-core: Fix database migration source

--- astronote-core/migrations/0001-create-note-table.rs
+++
@@ -1,8 +0,0 @@
-sqlx::migrate!(r#"
-    CREATE TABLE IF NOT EXISTS notes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        absolute_path TEXT NOT NULL UNIQUE,
-        next_datetime TEXT NOT NULL,
-        scheduler JSON NOT NULL
-    );
-"#);
