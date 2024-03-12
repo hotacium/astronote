571f6a7f0cc327345b1a1a29d1cd57cfe1866ba0
hmiya6 <h.miya.work@gmail.com>
2024-03-12 14:06:48 +0900

    astronote-core: Refactor: Use anyhow for better error handling

--- astronote-core/src/db/ron.rs
+++ astronote-core/src/db/ron.rs
@@ -1,27 +1,9 @@
+use anyhow::{anyhow, Context, Result};
 use std::fs::{self, read_dir, DirBuilder, File};
-use std::io::prelude::*;
+use std::io::{prelude::*, ErrorKind};
 use std::path::{Path, PathBuf};
 
 use crate::{Note, SerializedNote};
-
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
-
-#[derive(Debug)]
-pub enum Error {
-    FileNotExists(PathBuf),
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::FileNotExists(path) => {
-                write!(f, "Couldn't find the path: {:?}", path)
-            }
-        }
-    }
-}
-
-impl std::error::Error for Error {}
 
 pub struct NoteRepository {
     database_dir: PathBuf,
@@ -30,7 +12,12 @@ pub struct NoteRepository {
 impl NoteRepository {
     pub fn new(database_root: &Path) -> Result<Self> {
         if !database_root.exists() {
-            DirBuilder::new().recursive(true).create(database_root)?;
+            DirBuilder::new()
+                .recursive(true)
+                .create(database_root)
+                .with_context(|| {
+                    format!("Failed to create database directory in {database_root:?}")
+                })?;
         }
         Ok(Self {
             database_dir: PathBuf::from(database_root),
@@ -77,32 +64,48 @@ impl NoteRepository {
 
 fn write_metadata(note: Note, database_root: &Path) -> Result<()> {
     let metadata_path = get_metadata_path_from_note(&note, database_root);
-    // create parent directory if not exists
+    // create directory to store metadata under `database_root` if not exists
     let parent_dir_path = metadata_path
         .parent()
-        .ok_or(Error::FileNotExists(metadata_path.clone()))?;
+        .ok_or(anyhow!(
+            "Failed to get directory path of {:?}",
+            metadata_path
+        ))
+        .with_context(|| format!("Failed to get parent directory of {metadata_path:?}"))?;
     if !parent_dir_path.exists() {
-        DirBuilder::new().recursive(true).create(parent_dir_path)?;
+        DirBuilder::new()
+            .recursive(true)
+            .create(parent_dir_path)
+            .with_context(|| format!("Failed to create metadata directory: {parent_dir_path:?}"))?;
     }
     // convert note into ron string
-    let serialized_note: SerializedNote = note.try_into()?;
-    let ron = ron::ser::to_string_pretty(&serialized_note, ron::ser::PrettyConfig::default())?;
+    let serialized_note: SerializedNote = note
+        .try_into()
+        .with_context(|| "Failed to serialize note structure")?;
+    let ron = ron::ser::to_string_pretty(&serialized_note, ron::ser::PrettyConfig::default())
+        .with_context(|| "Failed to generate RON string from serialized object")?;
     // write into file
     let mut file = File::options()
         .read(true)
         .write(true)
         .create(true)
-        .open(metadata_path)?;
-    file.write_all(ron.as_bytes())?;
+        .open(metadata_path)
+        .with_context(|| "Failed to open or create metadata file: {metadata_path:?}")?;
+    file.write_all(ron.as_bytes())
+        .with_context(|| "Failed to write metadata to {metadata_path:?}")?;
     Ok(())
 }
 
 fn delete_metadata(note: Note, database_root: &Path) -> Result<()> {
     let metadata_path = get_metadata_path_from_note(&note, database_root);
     if !metadata_path.exists() {
-        return Err(Box::new(Error::FileNotExists(metadata_path)));
+        return Err(anyhow!(
+            "Failed to find metadata file to remove: {:?}",
+            metadata_path
+        ));
     }
-    fs::remove_file(metadata_path)?;
+    fs::remove_file(&metadata_path)
+        .with_context(|| format!("Failed to remove metadata file: {:?}", metadata_path))?;
     return Ok(());
 }
 
@@ -110,38 +113,50 @@ fn delete_metadata(note: Note, database_root: &Path) -> Result<()> {
 // 2. read each metadata file
 // 3. and then convert the content into Note
 fn read_metadata_from_directory(dir: &Path) -> Result<Vec<Note>> {
-    let pathes: Vec<PathBuf> = read_dir(dir)?
+    let pathes: Vec<PathBuf> = read_dir(dir)
+        .with_context(|| format!("Failed to read entries of {dir:?}"))?
         .into_iter()
         .map(|entry| Ok(entry?.path()))
-        .collect::<Result<Vec<PathBuf>>>()?;
+        .collect::<Result<Vec<PathBuf>>>()
+        .with_context(|| {
+            format!("There's some sort of intermittent IO error during reading directory: {dir:?}")
+        })?;
     let result = pathes
         .into_iter()
         .map(|path| {
             let result = if path.is_dir() {
                 read_metadata_from_directory(&path)?
             } else {
-                let note = read_metadata(&path)?;
+                let note = read_metadata(&path)
+                    .with_context(|| format!("Failed to read note metadata from {path:?}"))?;
                 vec![note]
             };
-            Result::Ok(result)
+            anyhow::Ok(result)
         })
         .try_fold(vec![], |mut acc, result_note_vec| {
             let note_vec = result_note_vec?;
             acc.extend(note_vec);
-            Result::Ok(acc)
+            anyhow::Ok(acc)
         })?;
     Ok(result)
 }
 
 fn read_metadata(filepath: &Path) -> Result<Note> {
     let ron_string: String = {
-        let mut file = File::open(&filepath)?;
+        let file = File::open(&filepath).with_context(|| format!("Failed to open {filepath:?}"))?;
         let mut buf = String::new();
-        let _size = file.read_to_string(&mut buf)?;
-        Result::Ok(buf)
+        // NOTE: metadata must be < 10 KiB; rather fail to prevent zipbomb
+        let _size = LimitReader::new(file, 10 * 1024)
+            .read_to_string(&mut buf)
+            .with_context(|| format!("Failed to read file content of {filepath:?}"))?;
+        anyhow::Ok(buf)
     }?;
-    let serialized_note: SerializedNote = ron::from_str(&ron_string)?;
-    let note: Note = serialized_note.try_into()?;
+    let serialized_note: SerializedNote = ron::from_str(&ron_string).with_context(|| {
+        format!("Failed to generate serialized metadata object from RON of {filepath:?}")
+    })?;
+    let note: Note = serialized_note
+        .try_into()
+        .with_context(|| "Failed to deserialize serialized metadata")?;
     Ok(note)
 }
 
@@ -157,6 +172,34 @@ fn get_metadata_path_from_path(path: &Path, database_root: &Path) -> PathBuf {
         .collect::<PathBuf>();
     path.set_extension("metadata");
     path
+}
+
+struct LimitReader<R: Read> {
+    reader: R,
+    limit: usize,
+}
+
+impl<R> LimitReader<R>
+where
+    R: Read,
+{
+    pub fn new(reader: R, limit: usize) -> Self {
+        Self { reader, limit }
+    }
+}
+
+impl<R> Read for LimitReader<R>
+where
+    R: Read,
+{
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        if buf.len() > self.limit {
+            return Err(std::io::Error::new(ErrorKind::Other, "too many bytes"));
+        }
+        let size = self.reader.read(buf)?;
+        self.limit -= size;
+        Ok(size)
+    }
 }
 
 #[cfg(test)]
